e00a9b0d3cff8cf94c9ee659c7e3218cbc044a7b
Enegadi Ismail <ismail0enegadi@gmail.com>
2024-03-18 03:42:06 +0000

    add pagination and add user form

--- src/main/java/dev/enegadi/jeetp3/Reposotories/PatientRepository.java
+++ src/main/java/dev/enegadi/jeetp3/Reposotories/PatientRepository.java
@@ -1,13 +1,15 @@
 package dev.enegadi.jeetp3.Reposotories;
 
 import dev.enegadi.jeetp3.Entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface PatientRepository extends JpaRepository<Patient,  Long> {
+public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     List<Patient> findPatientByName(String name);
 
@@ -15,4 +17,6 @@ public interface PatientRepository extends JpaRepository<Patient,  Long> {
     List<Patient> findPatientByQuery(@Param("x") String nameQuery);
 
     List<Patient> getPatientByScoreGreaterThan(int score);
+
+    Page<Patient> findByNameContains(String keyword, Pageable pageable);
 }
--- src/main/java/dev/enegadi/jeetp3/web/PatientController.java
+++ src/main/java/dev/enegadi/jeetp3/web/PatientController.java
@@ -3,9 +3,12 @@ package dev.enegadi.jeetp3.web;
 import dev.enegadi.jeetp3.Entities.Patient;
 import dev.enegadi.jeetp3.Reposotories.PatientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +18,12 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model){
-        List<Patient> patientsList = patientRepository.findAll();
-        model.addAttribute("patientsList", patientsList);
+    public String index(Model model, @RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "5") int size, @RequestParam(name = "keyword", defaultValue = "") String keyword) {
+        Page<Patient> patientsPage = patientRepository.findByNameContains(keyword, PageRequest.of(page, size));
+        model.addAttribute("patientsList", patientsPage.getContent());
+        model.addAttribute("pages", new int[patientsPage.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", keyword);
         return "patients";
     }
 }
